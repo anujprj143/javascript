271fe33c2d66a361c18272d6f7f16e56ed8d98bf
anujprj143 <dharmrp.143@gmail.com>
2020-04-11 17:20:36 +0530

    Loos Exercise first problem solved

--- Part8_Loops_Exercise.js
+++ Part8_Loops_Exercise.js
@@ -1,31 +1,37 @@
 /// PART 8 - LOOP EXERCISES
-// Before we continue on with your project, let's practice some loops!
-// You'll have just two problems, but you'll need to use each loop type we
+
+//Here I have just two problems, but you'll need to use each loop type we
 // learned about to solve them!
 
 
-///////////////////
-//// PROBLEM 1 ///
-/////////////////
 
-// Use a For Loop to print (console.log()) out the word "hello" 5 times.
+//// PROBLEM 1 ///
+
+// Using a For Loop , print (console.log()) out the word "hello" 5 times.
 //
-// Do this with a While Loop and a For Loop
+// I will  done this with a While Loop and a For Loop
 
 // While Loop
-
+var x=5
+while(x>0){
+        console.log("hello");
+        x=x-1;
+}
 
 // For Loop
 
 
+for (var i = 0; i < 5; i++) {
+        console.log("hello");
+}
 
 
-/////////////////
+
 // PROBLEM 2 ///
 ///////////////
 
-// Use Loops to console.log() (print out) all the odd numbers from 1 to 25
-// Do this using two methods, a while loop and a for loop
+// Using Loops to console.log() (print out) all the odd numbers from 1 to 25
+// I will do  this using two methods, a while loop and a for loop
 
 // METHOD ONE
 // While Loop
