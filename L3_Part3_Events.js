dd0d94c6441a481b98ac72ddcb9f46093eab4413
anujprj143 <dharmrp.143@gmail.com>
2020-04-18 20:21:43 +0530

    .js file to change the action on perticular event

--- L3_Part3_Events.js
+++ L3_Part3_Events.js
@@ -1,28 +1,30 @@
-
-
+//grabing the content having id one/two and three.
 var headOne = document.querySelector('#one')
 var headTwo = document.querySelector('#two')
 var headThree = document.querySelector('#three')
 
 // Hover (mouseover and mouseout)
+
+//'mouseover' executes when mouse is on that part
 headOne.addEventListener('mouseover',function(){
   headOne.textContent = "Mouse currently Over";
   headOne.style.color = 'red';
 })
 
+//it executes when mouse is not on that part
 headOne.addEventListener('mouseout',function(){
   headOne.textContent = "Mouse Not On me."
   headOne.style.color = 'blue';
 })
 
 
-// On Click
+// On Clicking the text will become blue
 headTwo.addEventListener("click",function(){
   headTwo.textContent = "Clicked On";
   headTwo.style.color = 'blue';
 })
 
-// Double Click
+//on Double Click text become red
 headThree.addEventListener("dblclick",function(){
   headThree.textContent = "Double Clicked!";
   headThree.style.color = 'red';
