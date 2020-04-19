8cd70f40dd1f8bae0f9945376ead688e4d8c02d9
anujprj143 <dharmrp.143@gmail.com>
2020-04-19 22:48:44 +0530

    Adding Event to change the content of squares using addEventListener

--- L3_Part4_Game.js
+++ L3_Part4_Game.js
@@ -26,3 +26,8 @@ function changeMarker(){
       this.textContent = '';
     }
 };
+
+// Use a for loop to add Event listeners to all the squares
+for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', changeMarker);
+}
