a7369d596e010487e60f45ccbc618b65ebfa188a
anujprj143 <dharmrp.143@gmail.com>
2020-04-19 22:46:56 +0530

    A function is created to change the mark on the square according to Game Plan

--- L3_Part4_Game.js
+++ L3_Part4_Game.js
@@ -13,3 +13,16 @@ function clearBoard() {
 
 }
 restart.addEventListener('click',clearBoard)
+
+
+// Create a function that will check the square marker
+function changeMarker(){
+    if(this.textContent === ''){
+      this.textContent = 'X';
+      // console.log(marker)
+    }else if (this.textContent === 'X') {
+      this.textContent = 'O';
+    }else {
+      this.textContent = '';
+    }
+};
