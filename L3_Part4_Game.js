983d25d9aca0d30e151cd591c63914aa4951f3c7
anujprj143 <dharmrp.143@gmail.com>
2020-04-19 22:40:01 +0530

    A clearboared function is created to clear the all text content

--- L3_Part4_Game.js
+++ L3_Part4_Game.js
@@ -3,3 +3,12 @@ var restart = document.querySelector('#b');
 
 // Grab all the squares
 var squares = document.querySelectorAll("td");
+
+
+// Clear Squares Function
+function clearBoard() {
+  for (var i = 0; i < squares.length; i++) {
+      squares[i].textContent = '';
+  }
+
+}
